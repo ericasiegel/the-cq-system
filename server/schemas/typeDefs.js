43154fdc1e807061416cefb1baedd288b727ff47
Erica Siegel <siegel.erica@gmail.com>
2021-07-05 10:13:48 -1000

    added schemas folder

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,8 @@
+// import the gql template function
+const { gql } = require('apollo-server-express');
+
+// create out typeDefs
+const typeDefs = gql``;
+
+// export the typeDefs
+module.exports = typeDefs;
